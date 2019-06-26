91a83b4642f90cbb485689bf04d430a27ffbd883
Student <example@example.com>
2019-06-26 15:00:47 -0700

    add cover to board for both players

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-inventory',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent implements OnInit {
-
+  @Input() color: string;
   constructor() { }
 
   ngOnInit() {
